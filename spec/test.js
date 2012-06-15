e1ef9fb59728fc5b430c79f027aca4caf828e550
Antony Denyer <antonydenyer@gmail.com>
2012-06-15 15:09:06 +0100

    more tests

--- spec/test.js
+++ spec/test.js
@@ -6,4 +6,34 @@ describe('Array', function(){
       [1,2,3].indexOf(3).should.equal(-1);
     });
   });
+  describe('#indexOf()', function(){
+    it('should return -1 when not present', function(){
+      [1,2,3].indexOf(3).should.equal(-1);
+    });
+  });
+  describe('#indexOf()', function(){
+    it('should return -1 when not present', function(){
+      [1,2,3].indexOf(3).should.equal(-1);
+    });
+  });
+  describe('#indexOf()', function(){
+    it('should return -1 when not present', function(){
+      [1,2,3].indexOf(3).should.equal(-1);
+    });
+  });
+  describe('#indexOf()', function(){
+    it('should return -1 when not present', function(){
+      [1,2,3].indexOf(3).should.equal(-1);
+    });
+  });
+  describe('#indexOf()', function(){
+    it('should return -1 when not present', function(){
+      [1,2,3].indexOf(3).should.equal(-1);
+    });
+  });
+  describe('#indexOf()', function(){
+    it('should return -1 when not present', function(){
+      [1,2,3].indexOf(3).should.equal(-1);
+    });
+  });
 });
