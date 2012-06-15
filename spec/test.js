cfbbd2b17f6952f4331b2135f730a1e6c9077463
Antony Denyer <antonydenyer@gmail.com>
2012-06-15 16:05:23 +0100

    more tests

--- spec/test.js
+++ spec/test.js
@@ -3,37 +3,37 @@ var should = require('chai').should();
 describe('Array', function(){
   describe('#indexOf()', function(){
     it('should return -1 when not present', function(){
-      [1,2,3].indexOf(3).should.equal(-1);
+      [1,2,3].indexOf(-1).should.equal(-1);
     });
   });
   describe('#indexOf()', function(){
     it('should return -1 when not present', function(){
-      [1,2,3].indexOf(3).should.equal(-1);
+      [1,2,3].indexOf(-1).should.equal(-1);
     });
   });
   describe('#indexOf()', function(){
     it('should return -1 when not present', function(){
-      [1,2,3].indexOf(3).should.equal(-1);
+      [1,2,3].indexOf(-1).should.equal(-1);
     });
   });
   describe('#indexOf()', function(){
     it('should return -1 when not present', function(){
-      [1,2,3].indexOf(3).should.equal(-1);
+      [1,2,3].indexOf(-1).should.equal(-1);
     });
   });
   describe('#indexOf()', function(){
     it('should return -1 when not present', function(){
-      [1,2,3].indexOf(3).should.equal(-1);
+      [1,2,3].indexOf(-1).should.equal(-1);
     });
   });
   describe('#indexOf()', function(){
     it('should return -1 when not present', function(){
-      [1,2,3].indexOf(3).should.equal(-1);
+      [1,2,3].indexOf(-1).should.equal(-1);
     });
   });
   describe('#indexOf()', function(){
     it('should return -1 when not present', function(){
-      [1,2,3].indexOf(3).should.equal(-1);
+      [1,2,3].indexOf(-1).should.equal(-1);
     });
   });
 });
